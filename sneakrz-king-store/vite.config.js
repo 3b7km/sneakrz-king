c56d886b22dcb80112435eef180c6f41ff59ebb5
3b7km <106720060+3b7km@users.noreply.github.com>
2025-06-20 03:12:13 +0300

    Track all files directly, remove submodule

---
+++ sneakrz-king-store/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      protocol: 'ws',
+      host: '5173-iwc2nvq4hrabqz32wj1pk-ed19f196.manusvm.computer',
+      clientPort: 443
+    },
+    watch: {
+      usePolling: true
+    }
+  },
+})
+
+
