15d83c98f2c02c6eef747735f1b561012268ffea
3b7km <106720060+3b7km@users.noreply.github.com>
2025-06-20 06:06:29 +0300

    Set base to /sneakrz-king/ in vite.config.js for GitHub Pages deployment

--- sneakrz-king-store/vite.config.js
+++ sneakrz-king-store/vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/sneakrz-king/', // Correct for GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
