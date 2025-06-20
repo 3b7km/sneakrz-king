6375a3591e731613a64b85c2427648b839455078
3b7km <106720060+3b7km@users.noreply.github.com>
2025-06-20 06:19:14 +0300

    Comment out base in vite.config.js for local dev, update dist output

--- sneakrz-king-store/vite.config.js
+++ sneakrz-king-store/vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/sneakrz-king/', // Correct for GitHub Pages
+  // base: '/sneakrz-king/', // Correct for GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
