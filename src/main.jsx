a5617751f3eb8d019f1a471ce1ae3dbc0d5f28e8
3b7km <106720060+3b7km@users.noreply.github.com>
2025-06-30 21:35:24 +0300

    the final inshaallah2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./ios-safari-fixes.css";
 import App from "./index.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </CartProvider>
   </StrictMode>,
 );
